ccb42e2ab097e88fbeec1e64e9eca3a6222eb4e0
Muhamad Adi Prasetyo <88260025+xTaMaox@users.noreply.github.com>
2023-01-07 18:54:13 +0700

    Add files via upload

---
+++ Gas Station.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int tank=0;
+        int total=0;
+        int index=0;
+
+        for(int i=0;i<gas.size();i++){
+            int consume=gas[i]-cost[i];
+            tank+=consume;
+            if(tank <0 ){
+                index=i+1;
+                tank=0;
+            }
+            total+=consume;
+        }
+
+        if(total <0) return -1;
+        else return index;
+    }
+};
